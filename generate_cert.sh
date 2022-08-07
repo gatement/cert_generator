ac3150840754d7a657534b45bef3fc80da3646b2
Johnson Lau <gatement@gmail.com>
2022-08-07 12:11:28 +0800

    go

--- generate_cert.sh
+++ generate_cert.sh
@@ -36,23 +36,33 @@ cd "${CERT_DIR}"
 ## 1. generate key and csr
 openssl genrsa -out key.pem 2048
 openssl req -new -sha512 -key key.pem -out req.pem -outform PEM -subj /CN="${CERT_NAME}"/ -nodes
+echo "--- key and csr generated."
 
 ## 2. sign by CA
 cd "../${CA_NAME}"
 # add subjectAltName for server certifiate
 cp openssl.cnf openssl_tmp.cnf
 echo "DNS.1 = ${CERT_NAME}" >> openssl_tmp.cnf
+#echo "[ v3_ca ]" >> openssl_tmp.cnf
+#echo "subjectAltName = IP:${CERT_NAME}" >> openssl_tmp.cnf
 openssl ca -md sha512 -config openssl_tmp.cnf -in "../${CERT_NAME}/req.pem" -out "../${CERT_NAME}/cert.pem" -notext -batch -extensions ${EXTENSIONS} 
 rm -rf openssl_tmp.cnf
+echo "--- signed by CA"
 
 ## 3. export and convert format
 cd "../${CERT_NAME}"
 
 cp cert.pem certs.pem
 cat ../${CA_NAME}/cacerts.pem >> certs.pem
+echo "--- certs.pem generated."
 
 openssl pkcs12 -export -out keycert.p12 -in cert.pem -inkey key.pem -passout pass:${PKCS12_PWD}
+echo "--- keycert.p12 generated."
+
 openssl pkcs12 -export -out keycerts.p12 -in certs.pem -inkey key.pem -passout pass:${PKCS12_PWD}
+echo "--- keycerts.p12 generated."
+
 openssl x509 -in cert.pem -out cert.cer -outform DER
+echo "--- cert.cer generated."
 
 cd ../..
